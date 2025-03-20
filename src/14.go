e4b4c15cb0d3b481f817de1a6c43763b8172fb44
runmaited <runmaited@gmail.com>
2025-03-20 17:25:59 +0000

    Add 14.go

---
+++ src/14.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    // Example usage of Go functions and variables
+    println("Hello, World!")
+    
+    name := "Alice"
+    age := 20
+    
+    // String formatting using format
+    fmt.Println(`Your name is ${name} and you are ${age} years old.`)
+}
